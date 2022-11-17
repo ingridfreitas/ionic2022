7de1b86ce9e896421c6df08f677b23466678545d
Ingrid Heloise <61015082+ingridfreitas@users.noreply.github.com>
2022-11-17 12:27:02 -0300

    Inicio de um sonho

--- log/src/app/cadastro/cadastro.page.ts
+++ log/src/app/cadastro/cadastro.page.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from '../model';
+import { ServicesService } from '../services.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroPage implements OnInit {
 
-  constructor() { }
+  nome: string;
+  cpf: string;
+  email: string;
+  senha: string;
+  cosenha: string
+
+  usuario: Usuario[]
+
+  user = {} as Usuario;
 
   ngOnInit() {
   }
 
+  constructor(private http: HttpClient, private router: Router, private service: ServicesService) {
+  }
+
+  salvar() {
+    const user: Usuario = {
+      nome: this.nome,
+      cpf: this.cpf,
+      email: this.email,
+      senha: this.senha
+    }
+
+    console.log(this.user.nome)
+    console.log(this.user.cpf)
+    console.log(this.user.email)
+    console.log(this.user.senha)
+
+
+    this.service.criarUsuario(user).subscribe(user => {
+      console.log(user)
+    })
+  }
+
+  
 }
--- log/src/app/login/login.page.ts
+++ log/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formLogin = this.formb.group({
+  email: ['', Validators.compose([Validators.required, Validators.email])], 
+  senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+})
+
+  constructor(private formb: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  entrar(){
   }
 
 }
